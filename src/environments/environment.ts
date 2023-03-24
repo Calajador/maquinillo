2e8b00603110db87a4042313f38bae0b6f92b05e
Curro <pandadormilon@gmail.com>
2023-03-24 23:01:23 +0100

    set config

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'maquinillo-ebdb5',
+    appId: '1:652195977693:web:b28d653da4f017523d7e34',
+    storageBucket: 'maquinillo-ebdb5.appspot.com',
+    apiKey: 'AIzaSyDlFPt-5UERVEClt0aZIDcPTfpb4X2mh14',
+    authDomain: 'maquinillo-ebdb5.firebaseapp.com',
+    messagingSenderId: '652195977693',
+  },};
